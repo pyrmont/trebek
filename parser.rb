4a570486d76ed7e3a4631cc0562638486c3a20fa
pyrmont <mike@inqk.net>
2012-07-21 01:14:19 +0900

    Example of what a survey should look like as well as the very beginnings of the parser.

---
+++ parser.rb
@@ -0,0 +1,82 @@
+class Parser
+	
+	def initialize
+
+	end
+
+	# Set up the parser with data that is going to be parsed.
+	def setup(data)
+		@data = data
+	end
+
+	def parse
+		# while lines have a line
+			# determine type of line
+			# if line is a survey
+				# create survey
+				# add to array of surveys
+				# if survey param unset
+					# set to survey param
+				# else
+					# raise error ('You have forgotten to close the previous survey.')
+			# else if line is end of survey
+				# if survey param set
+					# unset survey param
+				# else
+					# raise error ('You have forgotten to create a survey.')
+			# else if line is a group
+				# if group param set
+					# set group parent to be group param
+				# set to group param
+				# add to survey's children
+			# else if line is end of group
+				# if group param set
+					# if group parent set
+						# set group param to be group parent
+					# else
+						# unset group param
+					# add to survey's children
+				# else
+					# raise error ('You have forgotten to create a group or you have closed too many groups.')
+			# else if line is a table
+				# create table
+				# if survey param set
+					# add to survey's children
+				else
+					# raise error ('You haven't created a survey but you're trying to close one.')
+				# if table param unset
+					# set to table param
+				# else
+					# raise error ('You have forgotten to close the previous table.')
+			# else if line is end of table
+				# if table param set
+					# unset table param
+				# else
+					# raise error ('You haven't created a table yet but you're trying to close one.')
+			# else if line is part of a question
+				# if question param unset
+					# create question
+					# if table param set
+						# add to table's children
+					# else if survey param set
+						# add to survey's children
+					# else
+						# raise error ('You have forgotten to create a survey.')
+				# else if question param set and this part of the question set
+					# raise error ('You are trying to define the same part of the question twice.')
+				# add to question
+			# else if line is hr
+				# if table param is set
+					# add to table's children
+				# else if survey param is set
+					# add to survey's children
+				# else
+					# raise error ('You have forgotten to create a survey.')
+			# else if line is blank
+				# if question param set
+					# unset question param
+				# if question's question or heading is unset
+					# raise error ('You have forgotten to write a question or a heading to identify this question.')
+		# endwhile
+	end
+end
