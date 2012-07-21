fab09dd57defd8335f3f1f7f8c26a49544ae3a35
pyrmont <mike@inqk.net>
2012-07-21 10:27:55 +0900

    Modified the example survey and added a new one so I could test how it works without name. Modifications to the Tokeniser to be able to recognise survey tokens.

--- token.rb
+++ token.rb
@@ -1,15 +1,35 @@
 class Token
 
 	def initialize(type, line)
-
 		@type = type
-
 		@content = generate line
-
 	end
 
 	def generate(line)
+		result = nil
+		case @type
+		when :survey || :group || :table
+			case @type
+			when :survey
+				matches = line.scan(/Survey( \w*|)/i)
+			when :group
+				matches = line.scan(/Group( \w*|)/i)
+			when :table
+				matches = line.scan(/Table( \w*|)/i)
+			end
 
+			if name = matches[0][0]
+				result = (name != '') ? name.strip : nil
+			end
+
+			puts result.inspect
+		
+			result = nil
+		when :end_survey || :end_group || :end_table
+			result = nil
+		end
+
+		return result
 	end
 
 end
--- trebek.rb
+++ trebek.rb
@@ -2,7 +2,21 @@ require_relative 'Parser'
 
 data = ''
 
-File.open('surveys/simple.txt', 'r') do |survey|
+File.open('surveys/simple.names.txt', 'r') do |survey|
+	while line = survey.gets
+        data += line
+	end
+end
+
+parser = Parser.new
+parser.setup data
+parser.parse
+
+# Reset data
+puts "\n\n"
+data = ''
+
+File.open('surveys/simple.nonames.txt', 'r') do |survey|
 	while line = survey.gets
         data += line
 	end
