173c626449cacd4761eb5dfb5df9ba6adf3ccaee
Michael Camilleri <mike@inqk.net>
2014-07-25 11:39:30 +0900

    Added basic templating.

--- trebek.rb
+++ trebek.rb
@@ -1,3 +1,4 @@
+require 'erb'
 require './parser'
 
 # Use the first argument as the path to the file to be parsed (abort if there is a problem).
@@ -5,13 +6,22 @@ abort "Error: Trebek requires the path of the file to be parsed to be passed as 
 abort "Error: Trebek could not find the file at the path you specified." unless File.exists? filename
 
 # Get the contents of the file.
-contents = IO.read(filename)
+contents = File.read(filename)
 
 # Create the parser.
 parser = Parser.new
 
 # Parse the contents.
-parsed_contents = parser.parse contents
+@form = parser.parse contents
+
+# Create the ERB renderer.
+erb = ERB.new File.read('views/frame.erb')
+
+# Set the title of the page.
+@title = (ARGV[1]) ? ARGV[1] : 'Trebek Asks You the Questions!'
+
+# Insert the parsed contents into the HTML template.
+output = erb.result
 
 # Print out the modified content.
-puts parsed_contents
+puts output
